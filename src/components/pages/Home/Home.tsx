5fc08d59a913590108a2a8c9f981bbafbab6d143
RIERA Clément <clement.riera@orange.com>
2022-11-18 14:16:38 +0100

    fetchData

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -5,23 +5,19 @@ import Card from "../../Card/Card";
 
 import "./home.css";
 import { IMovie } from "../../Interfaces/MoviesInterfaces";
+import { fetchData } from "../../utils/FetchData";
 
 const Home = () => {
   const [discoverMovies, setDiscoverMovies] = useState<IMovie[]>([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    const getDicoverMovies = () => {
-      setLoading(true);
-      axios
-        .get(
-          "https://api.themoviedb.org/3/discover/movie?api_key=28b26ad998b3319aff99b90c0c534ba4&language=fr-fr&include_image_language=fr&sort_by=popularity.desc"
-        )
-        .then((response) => {
-          setLoading(false);
-          setDiscoverMovies(response.data.results);
-        });
-    };
-    getDicoverMovies();
+    const objState = {
+      setLoading,
+      mainState: setDiscoverMovies
+    }
+    const url = "https://api.themoviedb.org/3/discover/movie?api_key=28b26ad998b3319aff99b90c0c534ba4&language=fr-fr&include_image_language=fr&sort_by=popularity.desc"
+    fetchData(url, objState)
+
   }, []);
 
   return (
